ac6ec309de02d381b7f4b8722b64c1d83377e85e
jhyunwoo <jhyunwoo0228@gmail.com>
2023-07-14 20:31:04 +0900

    add home page

--- app/[dataId]/[areaId]/page.tsx
+++ app/[dataId]/[areaId]/page.tsx
@@ -1,4 +1,7 @@
+import dataIdToText from "@/lib/dataIdToText";
+import { ChevronDoubleLeftIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Area({
   params,
@@ -9,16 +12,24 @@ export default function Area({
   const decodedAreaId = decodeURI(params.areaId);
 
   return (
-    <div>
-      <div>{decodedDataId}</div>
-      <div>{decodedAreaId}</div>
+    <div className="bg-slate-50 p-4 w-full min-h-screen flex flex-col ">
+      <Link href={`/${decodedDataId}`} className="flex items-center py-2">
+        <ChevronDoubleLeftIcon className="w-6 h-6" />
+        <div className="text-sm">이전</div>
+      </Link>
+      <div className="text-xl font-bold mt-8">
+        {dataIdToText(decodedDataId)}
+      </div>
       <Image
         src={`/graph_images/${decodedDataId}/${decodedAreaId}.png`}
-        width={300}
-        height={300}
+        width={1000}
+        height={1000}
         alt={decodedDataId + decodedAreaId}
         priority={true}
-        // placeholder="blur"
+        quality={100}
+        className="w-full mx-auto"
+        blurDataURL="/loading.png"
+        placeholder="blur"
       />
     </div>
   );
